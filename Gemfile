e8d7dd79969281d3597955d63e9aee1f2679d8f4
Connor Ford <connor.ford@checkr.com>
2020-11-04 18:44:31 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby '2.6.5'
+
+gem 'mongoid', '=5.4.1'
+gem 'ddtrace', '=0.41.0'
---
+++ app.rb
@@ -0,0 +1,22 @@
+ENV['RACK_ENV'] = 'development'
+
+Bundler.require
+Mongoid.load!('./mongoid.yml')
+
+Datadog.configure do |c|
+  c.use(:mongo, service_name: 'mongo')
+end
+
+class MongoidDoc
+  include Mongoid::Document
+end
+
+# Any mongoid query
+MongoidDoc.new.save!
+
+# Sleep is neccesary to see segmentation fault output. 
+# Otherwise, different error: "~/.rubies/ruby-2.6.5/lib/ruby/2.6.0/timeout.rb:97: [BUG] sleeper must not be more than vm_living_thread_num(vm)"
+sleep(6)
+
+# If SystemExit is not raised, script finishes without error
+exit
